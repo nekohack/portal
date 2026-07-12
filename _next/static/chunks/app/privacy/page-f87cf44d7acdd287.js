756f6974d96afe27aa21b16fbff29bfbe891e53c
jiyuujin <jiyuujin@users.noreply.github.com>
2026-07-12 05:20:28 +0000

    deploy: 8ee3c39a930dc7d264139c0dffd7b86f0b07c1e2

---
+++ _next/static/chunks/app/privacy/page-f87cf44d7acdd287.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[877],{6132:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8591,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[591,119,245,358],()=>s(6132)),_N_E=e.O()}]);
